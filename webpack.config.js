341809e764f8dabb79cbbf071524ab80a5141021
dhollowe <dhollowe+odspmdb@microsoft.com>
2025-08-23 19:03:48 -0400

    also push webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isDevelopment = argv.mode === 'development';
@@ -52,6 +53,17 @@ module.exports = (env, argv) => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './public/index.html',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'public',
+            to: '',
+            globOptions: {
+              ignore: ['**/index.html'], // Don't copy index.html as HtmlWebpackPlugin handles it
+            },
+          },
+        ],
       }),
     ],
     devServer: {
